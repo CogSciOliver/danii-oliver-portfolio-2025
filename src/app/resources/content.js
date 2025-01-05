eceb702ba01a10e147dfdc5e4fd8de35519beb2b
Danii Oliver | Quantum Innovation Advisors <114320400+DTheArtist@users.noreply.github.com>
2025-01-05 13:46:59 -0600

    updated gallery

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -293,72 +293,72 @@ const gallery = {
 	// Images from https://pexels.com
 	images: [
 		{
-			src: "/images/gallery/img-01.jpg",
+			src: "/images/gallery/public/product-sdz-01.jpg",
 			alt: "image",
 			orientation: "vertical",
 		},
 		{
-			src: "/images/gallery/img-02.jpg",
+			src: "/images/gallery/project-mr-01.jpg",
 			alt: "image",
 			orientation: "horizontal",
 		},
 		{
-			src: "/images/gallery/img-03.jpg",
+			src: "/images/gallery/project-usd-01.jpg",
 			alt: "image",
 			orientation: "vertical",
 		},
 		{
-			src: "/images/gallery/img-04.jpg",
+			src: "/images/gallery/project-nyt-01.jpg",
 			alt: "image",
 			orientation: "horizontal",
 		},
 		{
-			src: "/images/gallery/img-05.jpg",
+			src: "/images/gallery/project-iso-01.jpg",
 			alt: "image",
 			orientation: "horizontal",
 		},
 		{
-			src: "/images/gallery/img-06.jpg",
+			src: "/images/gallery/project-iso-02.jpg",
 			alt: "image",
 			orientation: "vertical",
 		},
 		{
-			src: "/images/gallery/img-07.jpg",
+			src: "/images/gallery/project-iso-03.jpg",
 			alt: "image",
 			orientation: "horizontal",
 		},
 		{
-			src: "/images/gallery/img-08.jpg",
+			src: "/images/gallery/project-iso-04.jpg",
 			alt: "image",
 			orientation: "vertical",
 		},
 		{
-			src: "/images/gallery/img-09.jpg",
+			src: "/images/gallery/project-sdz-02.jpg",
 			alt: "image",
 			orientation: "horizontal",
 		},
 		{
-			src: "/images/gallery/img-10.jpg",
+			src: "/images/gallery/project-sdz-03.jpg",
 			alt: "image",
 			orientation: "horizontal",
 		},
 		{
-			src: "/images/gallery/img-11.jpg",
+			src: "/images/gallery/project-sdz-04.jpg",
 			alt: "image",
 			orientation: "vertical",
 		},
 		{
-			src: "/images/gallery/img-12.jpg",
+			src: "/images/gallery/project-sdz-05.jpg",
 			alt: "image",
 			orientation: "horizontal",
 		},
 		{
-			src: "/images/gallery/img-13.jpg",
+			src: "/images/gallery/project-sdz-06.jpg",
 			alt: "image",
 			orientation: "horizontal",
 		},
 		{
-			src: "/images/gallery/img-14.jpg",
+			src: "/images/gallery/project-wire-01.jpg",
 			alt: "image",
 			orientation: "horizontal",
 		},
