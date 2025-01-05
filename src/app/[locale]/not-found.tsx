c36781dddb61f7c06b70c9538c62d485689e9bda
Danii Oliver | Quantum Innovation Advisors <114320400+DTheArtist@users.noreply.github.com>
2025-01-05 08:47:27 -0600

    add 404 image for Opps meme

--- src/app/[locale]/not-found.tsx
+++ src/app/[locale]/not-found.tsx
@@ -1,24 +1,33 @@
 import { Flex, Heading, Text } from "@/once-ui/components";
 
 export default function NotFound() {
-    return (
-        <Flex
-            as="section"
-            direction="column" alignItems="center">
-            <Text
-                marginBottom="s"
-                variant="display-strong-xl">
-                404
-            </Text>
-            <Heading
-                marginBottom="l"
-                variant="display-strong-xs">
-                Page Not Found
-            </Heading>
-            <Text
-                onBackground="neutral-weak">
-                The page you are looking for does not exist.
-            </Text>
-        </Flex>
-    )
+	return (
+		<Flex
+			as="section"
+			direction="column"
+			alignItems="center"
+		>
+			<Image
+				src="public/images/404.webp"
+				width={500}
+				height={500}
+				alt="Picture of the author"
+			/>
+			<Text
+				marginBottom="s"
+				variant="display-strong-xl"
+			>
+				404
+			</Text>
+			<Heading
+				marginBottom="l"
+				variant="display-strong-xs"
+			>
+				Page Not Found
+			</Heading>
+			<Text onBackground="neutral-weak">
+				The page you are looking for does not exist.
+			</Text>
+		</Flex>
+	);
 }
