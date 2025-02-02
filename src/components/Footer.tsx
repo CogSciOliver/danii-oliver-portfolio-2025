80b569ad8c22454a01a978e541e952ec89964080
Danii Oliver | Quantum Innovation Advisors <114320400+DTheArtist@users.noreply.github.com>
2025-02-02 14:45:25 -0600

    footer update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,58 +1,72 @@
 import { renderContent } from "@/app/resources";
-import { Flex, IconButton, SmartLink, Text } from "@/once-ui/components"
+import { Flex, IconButton, SmartLink, Text } from "@/once-ui/components";
 import { useTranslations } from "next-intl";
-import styles from './Footer.module.scss'
+import styles from "./Footer.module.scss";
 
 export const Footer = () => {
-    const currentYear = new Date().getFullYear();
+	const currentYear = new Date().getFullYear();
 
-    const t = useTranslations();
-    const { person, social } = renderContent(t);
+	const t = useTranslations();
+	const { person, social } = renderContent(t);
 
-    return (
-        <Flex
-            as="footer"
-            position="relative"
-            fillWidth padding="8"
-            justifyContent="center" mobileDirection="column">
-            <Flex
-                className={styles.mobile}
-                fillWidth maxWidth="m" paddingY="8" paddingX="16" gap="16"
-                justifyContent="space-between" alignItems="center">
-                <Text
-                    variant="body-default-s"
-                    onBackground="neutral-strong">
-                    <Text
-                        onBackground="neutral-weak">
-                        © {currentYear} /
-                    </Text>
-                    <Text paddingX="4">
-                        {person.name}
-                    </Text>
-                    <Text onBackground="neutral-weak">
-                        {/* Usage of this template requires attribution. Please don't remove the link to Once UI. */}
-                        / View <SmartLink style={{marginLeft: '-0.125rem'}} href="https://quantuminnovationadvisors.com">Quantum Innovation Advisors'</SmartLink> Products.
-                    </Text>
-                    <Text paddingX="4">
-                        / Built with Once UI
-                    </Text>
-                </Text>
-                <Flex
-                    gap="16">
-                    {social.map((item) => (
-                        item.link && (
-                            <IconButton
-                                key={item.name}
-                                href={item.link}
-                                icon={item.icon}
-                                tooltip={item.name}
-                                size="s"
-                                variant="ghost"/>
-                        )
-                    ))}
-                </Flex>
-            </Flex>
-            <Flex height="80" show="s"></Flex>
-        </Flex>
-    )
-}
+	return (
+		<Flex
+			as="footer"
+			position="relative"
+			fillWidth
+			padding="8"
+			justifyContent="center"
+			mobileDirection="column"
+		>
+			<Flex
+				className={styles.mobile}
+				fillWidth
+				maxWidth="m"
+				paddingY="8"
+				paddingX="16"
+				gap="16"
+				justifyContent="space-between"
+				alignItems="center"
+			>
+				<Text
+					variant="body-default-s"
+					onBackground="neutral-strong"
+				>
+					<Text onBackground="neutral-weak">© {currentYear} /</Text>
+					<Text paddingX="4">{person.name}</Text>
+					<Text onBackground="neutral-weak">
+						{/* Usage of this template requires attribution. Please don't remove the link to Once UI. */}
+						/ Learn more about{" "}
+						<SmartLink
+							style={{ marginLeft: "-0.125rem" }}
+							href="https://quantuminnovationadvisors.com"
+						>
+							Quantum Innovation Advisors'
+						</SmartLink>
+						Technical Product Services.
+					</Text>
+					<Text paddingX="4">/ Built with Once UI</Text>
+				</Text>
+				<Flex gap="16">
+					{social.map(
+						(item) =>
+							item.link && (
+								<IconButton
+									key={item.name}
+									href={item.link}
+									icon={item.icon}
+									tooltip={item.name}
+									size="s"
+									variant="ghost"
+								/>
+							),
+					)}
+				</Flex>
+			</Flex>
+			<Flex
+				height="80"
+				show="s"
+			></Flex>
+		</Flex>
+	);
+};
